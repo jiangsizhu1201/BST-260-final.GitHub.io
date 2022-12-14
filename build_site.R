371ac7eab3a1c97041f62d480fd2ea72a3fd96bf
OS <jiangsizhu1201@gmail.com>
2022-12-14 15:45:59 -0500

    2nd

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/Nick/personal_site")
+setwd("C:/Users/OS/Desktop/hw_260/BST-260-final.GitHub.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
